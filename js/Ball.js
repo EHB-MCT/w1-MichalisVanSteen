dfda230ec2aaf72dbeacd1258dd083017d7191c2
Michalis Van Steen <michalis.van.steen@student.ehb.be>
2021-02-22 12:56:41 +0100

    toevoeging oef 4

--- js/Ball.js
+++ js/Ball.js
@@ -1,33 +1,53 @@
 "use strict";
 
 class Ball {
-    constructor(x, y, w, h) {
+    constructor(x, y, w, h, mass) {
         this.location = createVector(x, y);
         this.velocity = createVector(1, 1);
         this.acceleration = createVector(0, 0);
         this.w = w;
         this.h = h;
+        this.mass = mass;
     }
 
     draw() {
         stroke(0);
         fill(175);
-        circle(this.location.x, this.location.y, 50);
+        circle(this.location.x, this.location.y, this.mass * 2);
     }
 
     update() {
-        let mouse = createVector(mouseX, mouseY);
+        /*let mouse = createVector(mouseX, mouseY);
         mouse.sub(this.location.x, this.location.y);
         this.acceleration = mouse;
         this.acceleration.normalize();
-        this.acceleration.mult(1);
+        this.acceleration.mult(1);*/
+        this.edges();
 
         this.velocity.add(this.acceleration);
         this.velocity.limit(10);
         this.location.add(this.velocity);
     }
 
+    applyForce(force) {
+        let f = force.copy();
+        f.div(this.mass);
+        this.acceleration.add(f);
+    }
+
     edges() {
+        /*if(this.location.x > this.w) {
+            this.location.x = this.w;
+            this.velocity.x = this.velocity.x * -1;
+        }else if (this.location.x < 0) {
+            this.location.x = 0;
+            this.velocity.x = this.velocity.x * -1;
+        }
+        if(this.location.y > this.h) {
+            this.location.y = this.h;
+            this.velocity.y = this.velocity * -1;
+        }*/
+
         if ((this.location.x > this.w) || (this.location.x < 0)) {
             this.velocity.x = this.velocity.x * -1;
           }
--- js/W1-4-force.js
+++ js/W1-4-force.js
@@ -10,12 +10,31 @@
 
 const w = 800;
 const h = 800;
+let ball1, ball2;
 
 function setup() {
     createCanvas(w, h);
     background(200);
+    ball = new Ball(w/4, h/2, w, h, 10);
+    ball2 = new Ball(w/4, h/3, w, h, 20);
 }
 
 function draw() {
     background(200);
+
+    if(mouseIsPressed) {
+        let wind = createVector(5, 1);
+        ball1.applyForce(wind);
+        ball2.applyForce(wind);
+    }
+
+    let gravity = createVector(0, 3);
+    ball1.applyForce(gravity);
+    ball2.applyForce(gravity);
+
+    ball1.update();
+    ball2.update();
+
+    ball1.draw();
+    ball2.draw();
 }
