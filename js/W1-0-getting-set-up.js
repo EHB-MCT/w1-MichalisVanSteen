796c0b88c87fac35128a71b39b38f09dd5bd532d
Michalis Van Steen <michalis.van.steen@student.ehb.be>
2021-02-22 09:52:41 +0100

    Aanmaak PVector class

--- js/W1-0-getting-set-up.js
+++ js/W1-0-getting-set-up.js
@@ -11,6 +11,30 @@ let y = h/2;
 let xSpeed = 5;
 let ySpeed = 3;
 
+Vector location;
+Vector speed;
+
+class PVector {
+  float x;
+  float y;
+
+  PVector(float x_, float y_) {
+    x = x_;
+    y = y_;
+  }
+
+  void add(PVector v) {
+    y = y + v.y;
+    x = x + v.x;
+  }
+
+}
+
+PVector location = new PVector(100,100);
+PVector velocity = new PVector(1,3.3);
+
+location.add(velocity);
+
 function setup() {
     createCanvas(w, h);
     background(200);
